d6e7a2b7eea7defe5b4a9f641fb4b1d607847de1
Sofiya Chavarekar <sofiyachavarekar@gmail.com>
2026-07-05 21:14:37 +0530

    Add files via upload

---
+++ Assignment4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemainderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Sentence Reverser App ===");
+
+            StringReverser reverser = new StringReverser();
+
+            Console.Write("\nPlease type a sentence: ");
+            string userInput = Console.ReadLine();
+
+            string reversedResult = reverser.ReverseSentence(userInput);
+
+            Console.WriteLine("\n--- Results ---");
+            Console.WriteLine($"Original: {userInput}");
+            Console.WriteLine($"Reversed: {reversedResult}");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment4/StringReverser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemainderApp
+{
+    public class StringReverser
+    {
+        public string ReverseSentence(string sentence)
+        {
+            if (string.IsNullOrEmpty(sentence))
+            {
+                return sentence;
+            }
+
+            string reversed = "";
+
+            for (int i = sentence.Length - 1; i >= 0; i--)
+            {
+                reversed += sentence[i];
+            }
+
+            return reversed;
+        }
+    }
+}
