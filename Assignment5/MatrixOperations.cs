b5a2fef634ba46651d238b3aca3149e19cab6296
Sofiya Chavarekar <sofiyachavarekar@gmail.com>
2026-07-05 21:16:33 +0530

    Add files via upload

---
+++ Assignment5/MatrixOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixApp
+{
+    public class MatrixOperations
+    {
+        public int[,] AddMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int rows = matrixA.GetLength(0);
+            int cols = matrixA.GetLength(1);
+
+            int[,] matrixC = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+                }
+            }
+
+            return matrixC;
+        }
+
+        public void DisplayMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Assignment5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixOperations matOps = new MatrixOperations();
+
+            Console.Write("Enter the number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of columns: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] array1 = new int[rows, cols];
+            int[,] array2 = new int[rows, cols];
+
+            Console.WriteLine("\n--- Enter Elements for Matrix 1 ---");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Element [{i},{j}]: ");
+                    array1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\n--- Enter Elements for Matrix 2 ---");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Element [{i},{j}]: ");
+                    array2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] array3 = matOps.AddMatrices(array1, array2);
+
+            Console.WriteLine("\nMatrix 1:");
+            matOps.DisplayMatrix(array1);
+
+            Console.WriteLine("\nMatrix 2:");
+            matOps.DisplayMatrix(array2);
+
+            Console.WriteLine("\nSum (Stored in Matrix 3):");
+            matOps.DisplayMatrix(array3);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
