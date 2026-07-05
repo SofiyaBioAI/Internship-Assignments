78054610f812ed133ec831c84a7ad9e0560b80db
Sofiya Chavarekar <sofiyachavarekar@gmail.com>
2026-07-05 21:25:19 +0530

    Add files via upload

---
+++ Assignment6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee emp = new Employee(101, "John Doe");
+
+            
+            emp.Display();
+
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment6/ReadOnlyMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_ConsoleApp4
+{
+    public class Employee
+    {
+       
+        public int EmployeeId { get; }
+        public string Name { get; set; }
+
+        public Employee(int id, string name)
+        {
+            EmployeeId = id;
+            Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Employee ID: {EmployeeId}");
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+}
